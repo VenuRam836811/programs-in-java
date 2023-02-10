235e948314d843dffbe84ae79e6dcc37b0fd0bbc
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-10 10:21:13 +0530

    Create Solution4.java
    printing a given String for given number of times

---
+++ Solution4.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution4 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        String W = in.next();
+        for(int i=0;i<N;i++)
+        {
+ System.out.println(W);
+        }
+
+       
+
+       
+    }
+}
