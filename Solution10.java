076a11f5db2b1cebbbc549865c944e70ce4c2011
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-10 21:38:38 +0530

    printing odd numbers upto given number
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    9
    1
    3
    5
    7
    9
    02 Test 2
    Input
    Expected output
    101
    1
    3
    5
    7
    9
    11
    13
    15
    17
    19
    21
    23
    25
    27
    29
    31
    33
    35
    37
    39
    41
    43
    45
    47
    49
    51
    53
    55
    57
    59
    61
    63
    65
    67
    69
    71
    73
    75
    77
    79
    81
    83
    85
    87
    89
    91
    93
    95
    97
    99
    101
    03 Test 3
    Input
    Expected output
    20
    1
    3
    5
    7
    9
    11
    13
    15
    17
    19
    04 Test 4
    Input
    Expected output
    2
    1

---
+++ Solution10.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution10 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i=1;i<=N;i++)
+        {
+            if(i%2!=0)
+            {
+                System.out.println(i);
+            }
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+       // System.out.println("answer");
+    }
+}
