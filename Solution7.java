ac5dbebde6ba6435db6cd37a82b0e6868b7410c9
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-10 11:41:00 +0530

    number of trees can we plant
    hn's task is to plant as many trees as he can in a square grid
    One tree takes up 2x2 spaces
    Input
    An integer n Length of one side of a square
    Output
    An integer a The most number of trees John can plant
    Constraints
    2 < n < 50
    Example
    Input
    2
    Output
    1

---
+++ Solution7.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution7 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a=n/2;
+        int b=a*a;
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(b);
+    }
+}
