42544db5cebb147bc18efc47967c4b30f5a6bea8
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-10 11:30:52 +0530

    Create Solution6.java
    printing a number for reverse of that number

---
+++ Solution6.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution6 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            int X = in.nextInt();
+            int a=X+1;
+            int b=10-a;
+             System.out.println(b);
+        }
+
+       
+
+       
+    }
+}
