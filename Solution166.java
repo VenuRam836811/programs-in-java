625d0edcb54d3de27cd322c4f3867b84a0cdeefd
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-11 21:59:59 +0530

    Create Solution166.java
    NPUT:
    A single line containing the two angles a et b separated by a space.
    
    OUTPUT:
    A single line containing the last angle c.
    
    CONSTRAINTS:
    1≤a<180
    1≤b<180
    
    EXAMPLE:
    Input
    1 1
    Output
    178

---
+++ Solution166.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution166 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c=a+b;
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(180-c);
+    }
+}
