92229c7aeaafd6a2b9495fa9e747b1c30d172a0c
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-11 17:29:49 +0530

    adding one to given number

---
+++ Solution16.java
@@ -0,0 +1,6 @@
+import java.util.*;
+class Solution16 {
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n+1);}}
