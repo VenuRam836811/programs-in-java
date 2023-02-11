7450a85433f4f426b138e37e7f8fb31d17de8aab
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-11 10:35:21 +0530

    printing in a given formate
    You must print the string A surrounded by asterisks to form a box.
    Input
    A The string
    Output
    Line 1 Length of A+4 stars
    Line 2 A star followed by a space, then the string, another space and a final star.
    Line 3 Length of A+4 stars
    Constraints
    Length of A < 64
    Example
    Input
    Hello world!
    Output
    ****************
    * Hello world! *
    ****************

---
+++ Solution12.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution12 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String A = in.nextLine();
+        int b=A.length();
+        for(int i=0;i<b+4;i++)
+        {
+            System.out.print("*");
+        }
+        System.out.println();
+        System.out.print("*");
+        System.out.print(" ");
+        System.out.print(A);
+        System.out.print(" ");
+        System.out.println("*");
+        
+        
+        
+
+        for(int i=0;i<b+4;i++)
+        {
+            System.out.print("*");
+        }
+
+
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+       // System.out.println("SOLUTION");
+    }
+}
