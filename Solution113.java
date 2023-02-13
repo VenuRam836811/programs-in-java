07c7dd7ba2191d04197f6bed027cec2d7d523f64
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-13 07:32:13 +0530

    Create Solution113.java
    repeating the given first two characters into three times

---
+++ Solution113.java
@@ -0,0 +1,32 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution113 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String str = in.nextLine();
+        char ch='0';
+        for(int j=0;j<3;j++)
+        {
+        for(int i=0;i<2;i++)
+        {
+             ch=str.charAt(i);
+             System.out.printf("%c",ch);
+
+        }
+        }
+       // System.out.printf("%c",ch);
+
+
+        // Write an answer using System.out.println()
+        // To debug: System .err.println("Debug messages...");
+
+        
+    }
+}
