0e6926a29dc005c62651a9af092c3165fdd89e18
VenuRam836811 <124877728+VenuRam836811@users.noreply.github.com>
2023-02-17 14:22:22 +0530

    Create Solutiona3.java
    Bob is a farmer who lives in Axbourg, a small village of 100 inhabitants. He owns 7 chickens, 4 cows and 5 pigs. Each chicken lays in average 6 eggs per week. He has N eggs and Alice wants to buy at most M eggs. Display how many eggs Bob can sell to Alice.
    
    INPUT:
    Line 1 : The number N of eggs that Bob possess.
    Line 2 : The number M of eggs that Alice can buy.
    
    OUTPUT:
    An integer that is the number of eggs that Bob can sell to Alice.
    
    CONSTRAINTS:
    0 < N, M < 30
    
    EXAMPLE:
    Input
    5
    2
    Output
    2

---
+++ Solutiona3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solutiona3 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        int M = in.nextInt();
+        if(N>0)
+        {
+            System.out.println(N*M/N);
+        }
+        else if(N==0)
+        {
+            System.out.println(N);
+        }
+        
+
+       // System.out.println("answer");
+    }
+}
