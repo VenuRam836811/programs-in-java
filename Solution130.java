176e51c700a509d9fff9381a3496fb350c9a87e9
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-02-18 15:06:50 +0530

    print given number multiplied with given number-2
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    8
    48
    02 Test 2
    Input
    Expected output
    800
    638400
    03 Test 3
    Input
    Expected output
    100
    9800
    04 Test 4
    Input
    Expected output
    7
    35

---
+++ Solution130.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution130 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int A = in.nextInt();
+        int b=A-2;
+        System.out.println(A*b);
+    }
+}
