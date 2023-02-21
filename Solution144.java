3a4ad2f96347fbe39af776aea4a0a44aa5ed41e5
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-02-21 18:48:44 +0530

    print acconding to text case
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    5
    EEEEE
    02 Test 2
    Input
    Expected output
    17
    QQQQQQQQQQQQQQQQQ
    03 Test 3
    Input
    Expected output
    1
    A
    04 Test 4
    Input
    Expected output
    3
    CCC
    05 Test 5
    Input
    Expected output
    25
    YYYYYYYYYYYYYYYYYYYYYYYYY

---
+++ Solution144.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution144 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int a=n+64;
+        char c=(char)a;
+        for(int i=0;i<n;i++)
+        {
+            System.out.print(c);
+        }
+        
+    }
+}
