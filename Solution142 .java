96d2c9861f1fa98612b2fca0f433de0f09c47566
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-02-21 17:01:27 +0530

    Create Solution142 .java
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    3 37
    1
    02 Test 2
    Input
    Expected output
    4 91
    3
    03 Test 3
    Input
    Expected output
    5 156
    1
    04 Test 4
    Input
    Expected output
    13 41
    2

---
+++ Solution142 .java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution142 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N1 = in.nextInt();
+        int N2 = in.nextInt();
+        int a=N2%N1;
+        
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(a);
+    }
+}
