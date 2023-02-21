0a3426b7c41ca277be1ae935132ebd22ac8c9ac0
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-02-21 19:16:34 +0530

    write the program as for the following text cases
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    1
    1
    02 Test 2
    Input
    Expected output
    10
    12345678910123456789101234567891012345678910123456789101234567891012345678910123456789101234567891012345678910
    03 Test 3
    Input
    Expected output
    12
    123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112123456789101112
    04 Test 4
    Input
    Expected output
    6
    123456123456123456123456123456123456

---
+++ H146.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class H146 {
+
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=1;j<=n;j++)
+            {
+               System.out.print(j);
+            }
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+       // System.out.println("answer");
+    }
+}
