85806cfbfc305d4121628d7caaddfa742bf0c5a2
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-02-22 18:58:32 +0530

    n a town there are X men, each man has X wives and each wife has X kids, you should print the total T number of people in this town.
    n a town there are X men, each man has X wives and each wife has X kids, you should print the total T number of people in this town.
    Input
    One line: The integer X
    Output
    One line: The total T number of people in this town
    Constraints
    0 ≤ X ≤ 300
    Example
    Input
    7
    Output
    399

---
+++ Solution151.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution151 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+        int a=x*x*(x+1)+x;
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(a);
+    }
+}
