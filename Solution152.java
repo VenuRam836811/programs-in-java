c58432cef90dc92c76284211a44e0398901d59ae
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-02-22 19:09:49 +0530

    square of given number to number+1
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    1
    1
    02 Test 2
    Input
    Expected output
    2
    8
    03 Test 3
    Input
    Expected output
    3
    81
    04 Test 4
    Input
    Expected output
    4
    1024
    05 Test 5
    Input
    Expected output
    5
    15625

---
+++ Solution152.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution152 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        long A = in.nextLong();
+        double B=Math.pow(A,A+1);
+        int c=(int)B;
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(c);
+    }
+}
