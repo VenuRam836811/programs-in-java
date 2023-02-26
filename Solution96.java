5699f9935535d78deb21cf8ffda31b3f0ca4f629
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-02-26 20:36:42 +0530

    convert a decimal number to a binary string
    The program:
    Your program must output the binary representation of each number given as input.
    Each number will be given on a separate line, you must output the binary result followed by a newline character in the same order.
    The binary representation must always start with a 1, except for the number 0 (no zero-padding).
    
    INPUT:
    Line 1: N, an integer for the amount of numbers to test.
    N next lines: X, an integer.
    
    OUTPUT:
    N lines: The binary representation of X.
    
    CONSTRAINTS:
    0 < N ≤ 100
    0 ≤ X ≤ 1000
    
    EXAMPLE:
    Input
    6
    0
    1
    2
    3
    9
    256
    Output
    0
    1
    10
    11
    1001
    100000000

---
+++ Solution96.java
@@ -0,0 +1,26 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution96 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for (int i = 0; i < N; i++) {
+            int X = in.nextInt();
+            String a=Integer.toBinaryString(X);
+            System.out.println(a);
+
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        
+    }
+}
