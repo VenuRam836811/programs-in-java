51ea48e7875110b6d6a9f501d5d78750cd731107
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-03-05 12:35:53 +0530

    multiply,add and subtract the given numbers
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    8 2
    16106
    02 Test 2
    Input
    Expected output
    5 4
    2091
    03 Test 3
    Input
    Expected output
    9 6
    54153
    04 Test 4
    Input
    Expected output
    20 3
    602317

---
+++ Arthimatic.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Arthimatic {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.print(a*b);
+         System.out.print(a+b);
+          System.out.print(a-b);
+    }
+}
