c543573554142a66f0bfdd2f62246421edf63764
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-03-07 21:53:04 +0530

    squaring of first number and multiplied with next number
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    2 
    4
    16
    02 Test 2
    Input
    Expected output
    7 
    11
    539
    03 Test 3
    Input
    Expected output
    456 
    54
    11228544
    04 Test 4
    Input
    Expected output
    1111 
    79
    97511359
    05 Test 5
    Input
    Expected output
    0 
    6
    0

---
+++ Solution89.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution89 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int M = in.nextInt();
+        int N = in.nextInt();
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println(M*M*N);
+    }
+}
