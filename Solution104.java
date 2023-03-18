977079a2073320221413d893acd66eac0e4b82ed
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-03-18 18:48:33 +0530

    form a reverse pyramid
    Given a digit N, you must print a reverse pyramid with the base containing N times that digit. Each consecutive row containing 1 digit less.
    The result should contain no spaces.
    
    INPUT:
    Line 1: An integer N.
    
    OUTPUT:
    A triangle formed by N times the digit N one the first row, then one digit less per row.
    
    CONSTRAINTS:
    1 ≤ N ≤ 9
    
    EXAMPLE:
    Input
    2
    Output
    22
    2

---
+++ Solution104.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution104 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int N = in.nextInt();
+        for(int i=0;i<N;i++)
+        {
+            for(int j=i;j<N;j++)
+            {
+                System.out.print(N);
+            }
+            System.out.println();
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        
+    }
+}
