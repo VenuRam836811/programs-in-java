77dddb2bbc23c19665a7c351f889d1b2b1155561
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-03-24 08:14:33 +0530

    print character and space in a string and remove all other
    Some strings are just too complicated, they would be much simpler if they didn't have so many different characters.
    
    Remove all characters from the given string, except for letters and spaces.
    Input
    A single line of text.
    Output
    A single line of text containing alpha characters and spaces.
    Constraints
    Text will be printable ASCII.
    Example
    Input
    Hello, World!
    Output
    Hello World

---
+++ Solve114.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solve114 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String s = in.nextLine();
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(Character.isLetter(ch) || ch==' ')
+            {
+                System.out.print(ch);
+            }
+            else
+            {
+
+            }
+            
+        }
+
+    }
+}
