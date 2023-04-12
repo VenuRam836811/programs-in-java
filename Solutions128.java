d1506d41749a73a53a9f841aff64df54e4989a9f
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-04-12 21:23:49 +0530

    The minimum number of splits necessary to divide the bar into 1*1 units
    I have a chocolate bar of size m*n. I wish to split it into unit-sized pieces. What is the minimum split I have to do with the bar?
     _ _      _   _      _   _   _   _
    |_|_| => |_| |_| => |_| |_| |_| |_|
    |_|_|    |_| |_|
    Example For a chocolate bar of size 2*2, after the first split, it becomes 2 bars of 1*2. It needs the second and third splits on each smaller bar to divide them into 4 units. Once a bar is separated, I cannot stack them up to split them together. I have to handle them separately.
    Minimum split is 3
    Input
    Line 1: Space-separated integers M and N, representing the width and height of the chocolate bar.
    Output
    The minimum number of splits necessary to divide the bar into 1*1 units
    Constraints
    1 ≤ M, N ≤ 100,000
    Example
    Input
    2 2
    Output
    3

---
+++ Solutions128.java
@@ -0,0 +1,7 @@
+import java.util.*;
+class Solutions128{
+public static void main(String args[]) {
+Scanner in = new Scanner(System.in);
+int M = in.nextInt();
+int N = in.nextInt();
+System.out.println(M*N-1);}}
