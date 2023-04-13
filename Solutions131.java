1d50246d4c3b6c7cd5ccc75ec919530008f45464
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-04-13 22:17:41 +0530

    The number of regular n-unit triangles inside the regular hexagon
    Parse the regular hexagon, and tell how much regular unit triangle do you find, if the length of one side of the regular hexagon is n unit(s).
    
    As it is well known, every regular hexagon, that has a positive integer value for the length of one side can be split into a positive integer value of regular triangles, where the regular triangle has sides with 1 unit of length.
    
    For example, if the hexagon has sides with 1 unit of length, it will be split into 6 triangles.
    If the hexagon has sides with 2 units of length, it will be split into 24 triangles.
    Input
    Line 1: The length of one side of the hexagon is n unit
    Output
    Line 1 : The number of regular 1-unit triangles inside the regular hexagon
    Constraints
    0 < n < 7500
    Example
    Input
    1
    Output
    6

---
+++ Solutions131.java
@@ -0,0 +1,8 @@
+import java.util.*;
+class Solutions131 {
+public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        System.out.println(n*n*6);
+    }
+}
