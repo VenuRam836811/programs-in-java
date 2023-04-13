7ec7bd55b6d9e40b475dfb6c73aa8008f89f7858
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-04-13 21:25:44 +0530

    print the given number in a right angle triangle
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    1
    1
    02 Test 2
    Input
    Expected output
    2
    2
    22
    03 Test 3
    Input
    Expected output
    5
    5
    55
    555
    5555
    55555
    04 Test 4
    Input
    Expected output
    6
    6
    66
    666
    6666
    66666
    666666
    05 Test 5
    Input
    Expected output
    9
    9
    99
    999
    9999
    99999
    999999
    9999999
    99999999
    999999999

---
+++ Solution128.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution128 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            for(int j=0;j<=i;j++)
+            {
+                System.out.print(n);
+            }
+            System.out.println();
+        }
+    }
+}
