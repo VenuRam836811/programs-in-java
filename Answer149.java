eb50a1f400adbcc5c0dd9cedbac955e660da37ac
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-04-17 07:56:37 +0530

    The total number of integers leading up to n whose digits have a sum of 10
    When given an integer n, output the total number of integers leading up to n whose digits have a sum of 10.
    
    For example, 19 when taken as 1 + 9 adds up to 10, and is thus considered valid.
    
    Another example:
    Input: 50
    
    19 (1 + 9 = 10)
    28 (2 + 8 = 10)
    37 (3 + 7 = 10)
    46 (4 + 6 = 10)
    
    Output: 4 (4 integers that met the criteria were found)
    Input
    An integer n representing the number of integers to check.
    Output
    The total number of integers leading up to n whose digits have a sum of 10.
    Constraints
    10 ≤ n ≤ 1,000,000
    Example
    Input
    100
    Output
    9

---
+++ Answer149.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Answer149 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int count = 0;
+        for (int i = 1; i <= n; i++) {
+            int sum = 0;
+            int num = i;
+            while (num > 0) {
+                sum += num % 10;
+                num /= 10;
+            }
+            if (sum == 10) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
+
+
+
