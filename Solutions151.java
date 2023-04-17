40593dbc78b3b5b5643b5d017b3891579cfaa71e
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-04-17 08:04:14 +0530

    print the ladder using # symbol
    Simon the firefighter has to rescue a family from a burning building. The family is located on an upper floor, but unfortunately the fire has already spread and the stairs are not accessible anymore.
    Build a ladder for Simon so he can reach the family and rescue them.
    
    For every floor you have to print 1 ladder segment. 1 segment looks like this:
    
    #  #
    ####
    #  #
    
    Two segments look like this:
    
    #  #
    ####
    #  #
    ####
    #  #
    Input
    An integer floor, on which the family is located.
    Output
    The ASCII ladder.
    Constraints
    1 <= floor <= 32
    Example
    Input
    2
    Output
    #  #
    ####
    #  #
    ####
    #  #

---
+++ Solutions151.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solutions151 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int floor = in.nextInt();
+        for(int i=0;i<floor;i++)
+        {
+            System.out.println("#  #");
+            System.out.println("####");
+        }
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println("#  #");
+    }
+}
