e0914f11b14c0a0e8033270cf934d086a5d274b5
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-04-21 21:40:49 +0530

    the digital root of given number
    The digital root is obtained by summing the individual digits of the number, then repeating the process on the result until the result has a single digit.
    
    Special case: the digit root of 0 is 0.
    
    For instance:
    - the digital root of 12 is 3 (1 + 2)
    - the digital root of 85 is 4 (8 + 5 = 13 -> 1 + 3 = 4)
    Input
    Line 1: N, the number to process, greater than 0
    Output
    Line 1: the digital root of N
    Constraints
    0<=N<2^32
    Example
    Input
    12
    Output
    3

---
+++ Solutions166.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solutions166 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        long n = in.nextLong();
+        long sum=0,sum1=0;
+        long digitalRoot = n == 0 ? 0 : (n % 9 == 0 ? 9 : n % 9);
+        System.out.println(digitalRoot);
+
+
+        
+    }
+}
