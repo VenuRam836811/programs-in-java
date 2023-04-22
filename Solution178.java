c30edc6e09ee4255eeb9cd7b430c64ec060ce6bb
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-04-22 21:08:40 +0530

    The number of the day of the week of Tom's birthday.
    Tom's birthday is in N days. To prepare a thoughtful party, Tom needs to know the day of the week of his birthday. Let's help him!
    Days are numbered 1 to 7 (Sunday is 1, Monday is 2,...)
    Input
    First Line, m is today's day of the week
    Next Line, n days until Tom's birthday
    Output
    The number of the day of the week of Tom's birthday.
    Constraints
    1<=m<=7
    n>0
    Example
    Input
    3
    4
    Output
    7

---
+++ Solution178.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution178 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int m = in.nextInt();
+        int n = in.nextInt();
+        int a=m+n;
+        if(a<=7)
+         System.out.println(a);
+         else
+          System.out.println(a-7);
+    }
+}
