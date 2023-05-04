e396a569d95a989c40279515b85cfd11da345ef5
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-05-04 19:39:06 +0530

    print 1st string upto length of the 2nd string
    The game mode is REVERSE: You do not have access to the statement. You have to guess what to do by observing the following set of tests:
    01 Test 1
    Input
    Expected output
    asdfa afdas
    asdfaasdfaasdfaasdfaasdfa
    02 Test 2
    Input
    Expected output
    qwqqwq qwq
    qwqqwqqwqqwqqwqqwq
    03 Test 3
    Input
    Expected output
    j lkajsdflkjdslkg
    jjjjjjjjjjjjjjj
    04 Test 4
    Input
    Expected output
    input output
    inputinputinputinputinputinput
    05 Test 5
    Input
    Expected output
    123oiuO*#@ **U*qq9R
    123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@123oiuO*#@

---
+++ Solution204.java
@@ -0,0 +1,21 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution204 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        String a = in.next();
+        String b = in.next();
+        for(int i=0;i<b.length();i++)
+        {
+            System.out.print(a);
+        }
+
+    }
+}
