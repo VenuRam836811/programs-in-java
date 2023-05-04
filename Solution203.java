f509e80469b6ffec33bc8ea31a158f7aa3225542
venu gopal reddy <124877728+VenuRam836811@users.noreply.github.com>
2023-05-04 19:33:47 +0530

    the maximum number of intersecting points
    You are given a number of circles assuming that none are positioned on top of each other. What is the most number of points that they will intersect at? For example 3 circles: 6 points
    Input
    One int x the number of circles
    Output
    One int y the maximum number of intersecting points
    Constraints
    1<x<1000000000
    Example
    Input
    3
    Output
    6

---
+++ Solution203.java
@@ -0,0 +1,20 @@
+import java.util.*;
+import java.io.*;
+import java.math.*;
+
+/**
+ * Auto-generated code below aims at helping you parse
+ * the standard input according to the problem statement.
+ **/
+class Solution203 {
+
+    public static void main(String args[]) {
+        Scanner in = new Scanner(System.in);
+        int x = in.nextInt();
+
+        // Write an answer using System.out.println()
+        // To debug: System.err.println("Debug messages...");
+
+        System.out.println((long)x*x-x);
+    }
+}
